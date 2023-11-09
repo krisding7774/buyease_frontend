ebf9a4d5ce9d2557699bb8a0952d1c8a6fec3349
ASGARD\ricky_wang <Ricky_Wang@asgard.com.tw>
2023-11-09 16:08:34 +0800

    Feature: Add router elementui and vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+const app = createApp(App);
+app.use(ElementPlus).use(store).use(router).mount("#app");
+
+export default app;
