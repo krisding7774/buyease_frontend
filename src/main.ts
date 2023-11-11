7388c804f990f86b776d5ce0566765198f233608
Ricky <ricky7774@gmail.com>
2023-11-11 14:14:00 +0800

    add bootstrap and remove element-ui

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 
 const app = createApp(App);
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
-}
-
-app.use(ElementPlus).use(store).use(router).mount("#app");
+app.use(store).use(router).mount("#app");
 
 export default app;
