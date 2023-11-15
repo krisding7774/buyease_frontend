eeec0e9b7c2fa01ae82f2b0ae437f11b7b666b32
ASGARD\ricky_wang <Ricky_Wang@asgard.com.tw>
2023-11-15 15:38:04 +0800

    Test to add Layout

---
+++ src/views/NavBarLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header></Header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "../components/Header.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Layout",
+  components: {
+    Header,
+  },
+});
+</script>
